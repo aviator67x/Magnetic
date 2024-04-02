d778bd82cbc891b0804face7775fe1cdbd1ff4ac
Andrew Kasilov <aviator67x@gmail.com>
2024-04-02 18:21:39 +0200

    [update] magnetic view

--- Magnetic/MagneticModule/MagneticView.swift
+++ Magnetic/MagneticModule/MagneticView.swift
@@ -30,12 +30,16 @@ final class MagneticView: UIView {
     }
     
     // - MARK: Views
-    private let imageView = UIImageView()
+    private let magnetView = UIImageView()
+    private let dashboardView = UIImageView()
+    private let arrowView = UIImageView()
+    private let magnetismLabel = UILabel()
     private let button = UIButton()
     
     // - MARK: Private properties
     private(set) lazy var actionPublisher = actionSubject.eraseToAnyPublisher()
     private let actionSubject = PassthroughSubject<MagneticViewActions, Never>()
+    private let screenSize: CGRect = UIScreen.main.bounds
     private var buttonState: ButtonState = .search
     
     // - MARK: Lifecycle
@@ -53,14 +57,14 @@ final class MagneticView: UIView {
     func startSearch() {
         UIImageView.animate(withDuration: 2) {
             let transformation = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 0.5))
-            self.imageView.transform = transformation
+            self.arrowView.transform = transformation
         }
     }
     
     func stopSearch() {
         UIImageView.animate(withDuration: 0.5) {
             let transformation = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 0.0))
-            self.imageView.transform = transformation
+            self.arrowView.transform = transformation
         }
     }
 
@@ -70,32 +74,60 @@ final class MagneticView: UIView {
         buttonState == .search ? startSearch() : stopSearch()
         buttonState.toggle()
         button.setTitle(buttonState == .search ? "Seearch" : "Stop", for: .normal)
+        magnetismLabel.text = buttonState == .search ? "Search checking" : "50 µT"
     }
 }
 
 // - MARK: private extension
 private extension MagneticView {
     func setupViews() {
-        imageView.image = UIImage(named: "red")
+        magnetView.image = UIImage(named: "magnet")
+        dashboardView.image = UIImage(named: "dashboard")
+        
+        arrowView.image = UIImage(named: "arrow")
+        let xOffset = arrowView.frame.size.width / 2
+        let yOffset = arrowView.frame.size.height / 2
+//        arrowView.layer.anchorPoint = CGPoint(x: xOffset, y: 0)
+//        arrowView.layer.anchorPoint = CGPoint(x: xOffset, y: yOffset)
+//        arrowView.setAnchorPoint(CGPoint(x: xOffset, y: 0))
+        
+        magnetismLabel.text = buttonState == .search ? "Search checking" : "50 µT"
+        magnetismLabel.font = UIFont.systemFont(ofSize: 17)
+        magnetismLabel.textColor = .white
         
         button.addTarget(self, action: #selector(onButtonTap), for: .touchUpInside)
-        button.backgroundColor = .red
-        button.setTitle(buttonState == .search ? "Seearch" : "Stop", for: .normal)
+        button.backgroundColor = UIColor(named: "purple")
+        button.setTitle(buttonState == .search ? "Search" : "Stop", for: .normal)
+        button.layer.cornerRadius = 25
     }
     
     func setupLayout() {
-        addSubview(imageView)
-        imageView.snp.makeConstraints { make -> Void in
-            make.center.equalToSuperview()
-            make.size.equalTo(100)
+        addSubview(magnetView) {
+            $0.leading.top.trailing.equalToSuperview()
+            $0.height.equalTo(self.screenSize.width / 1.2)
+        }
+        addSubview(dashboardView) {
+            $0.top.equalTo(magnetView.snp.bottom).offset(50)
+            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(self.screenSize.width / 1.9)
         }
         
-        addSubview(button)
-        button.snp.makeConstraints { make -> Void in
-            make.centerX.equalToSuperview()
-            make.height.equalTo(50)
-            make.width.equalTo(150)
-            make.top.equalTo(imageView.snp.bottom).offset(50)
+        addSubview(arrowView) {
+            $0.centerX.equalToSuperview().offset(-40)
+            $0.centerY.equalTo(dashboardView.snp.bottom)
+        }
+        
+        addSubview(magnetismLabel) {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalTo(arrowView.snp.bottom).offset(50)
+        }
+        
+        addSubview(button) {
+            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(50)
+            $0.bottom.equalToSuperview().inset(50)
         }
     }
 }
